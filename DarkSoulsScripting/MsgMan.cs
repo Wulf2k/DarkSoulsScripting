6f3f9b0f6a7cad8698c87aa3b569baaed8a23667
Wulf2k <wulf2k@gmail.com>
2018-11-09 21:10:40 -0600

    MsgMan updates

--- DarkSoulsScripting/MsgMan.cs
+++ DarkSoulsScripting/MsgMan.cs
@@ -13,11 +13,63 @@ namespace DarkSoulsScripting
         //DSR 1.03
 
         public static IntPtr Address => RIntPtr(0x141d1b748);
+        public static Fmg DlgMsg { get; private set; } = null;
+        public static Fmg SysMsg { get; private set; } = null;
+        
 
         static MsgMan()
+        {
+            DlgMsg = new Fmg() { AddressReadFunc = () => RIntPtr(Address + 0x338) };
+            SysMsg = new Fmg() { AddressReadFunc = () => RIntPtr(Address + 0x340) };
+        }
+
+    }
+
+    public class Fmg : GameStruct
+    {
+        protected override void InitSubStructures()
         {
 
         }
 
+
+        public IntPtr getOffset(int id)
+        {
+            int numEntries;
+            int startOffset;
+            int startIdx;
+            int startId;
+            int endId;
+            numEntries = RInt32(Address + 0xC);
+            startOffset = RInt32(Address + 0x14);
+            for (int i = 0; i < numEntries; i++)
+            {
+                startIdx = RInt32(Address + 0x1c + i * 0xC);
+                startId = RInt32(Address + 0x1c + i * 0xC + 4);
+                endId = RInt32(Address + 0x1c + i * 0xC + 8);
+
+                if ((id >= startId) && (id <= endId))
+                {
+                    return Address + RInt32(Address + startOffset + 4 * (startIdx + id - startId));
+                }
+            }
+            return IntPtr.Zero;
+            
+        }
+
+        public string Msg(int id)
+        {
+            IntPtr txtLoc = getOffset(id);
+            if (txtLoc.Equals(IntPtr.Zero))
+                { return ""; }
+            return RUnicodeStr(txtLoc, 0x50);
+        }
+        public void Msg(int id, string str)
+        {
+            IntPtr txtLoc = getOffset(id);
+            if (!((long)txtLoc > (long)Address))
+            { return; }
+            WBytes(txtLoc, System.Text.Encoding.Unicode.GetBytes(str + '\0'));
+        }
     }
 }
